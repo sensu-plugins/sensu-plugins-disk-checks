e5f57b909a12780574b8313f77abd70532c743a9
Matt Jones <mjones@yieldbot.com>
2015-02-03 09:37:48 -0500

    Add new disk-usage check
    The check is a pure-ruby partial drop in for check-disk.  It will function the same but lacks the ignoreline and includeline pattern matching abilities at this time.

---
+++ bin/check-disk-usage.rb
@@ -0,0 +1,140 @@
+#! /usr/bin/env ruby
+#
+#   check-disk
+#
+# DESCRIPTION:
+#   Uses the sys-filesystem gem to get filesystem mount points and metrics
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Linux, BSD, Solaris, Windows
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#   gem: sys-filesystem
+#
+# USAGE:
+#
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2015 Yieldbot Inc <devops@yieldbot.com>
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+require 'sensu-plugin/check/cli'
+require 'sys/filesystem'
+include Sys
+
+#
+# Check Disk
+#
+class CheckDisk < Sensu::Plugin::Check::CLI
+  option :fstype,
+         short: '-t TYPE[,TYPE]',
+         description: 'Only check fs type(s)',
+         proc: proc { |a| a.split(',') }
+
+  option :ignoretype,
+         short: '-x TYPE[,TYPE]',
+         description: 'Ignore fs type(s)',
+         proc: proc { |a| a.split(',') }
+
+  option :ignoremnt,
+         short: '-i MNT[,MNT]',
+         description: 'Ignore mount point(s)',
+         proc: proc { |a| a.split(',') }
+
+  option :bwarn,
+         short: '-w PERCENT',
+         description: 'Warn if PERCENT or more of disk full',
+         proc: proc(&:to_i),
+         default: 85
+
+  option :bcrit,
+         short: '-c PERCENT',
+         description: 'Critical if PERCENT or more of disk full',
+         proc: proc(&:to_i),
+         default: 95
+
+  option :iwarn,
+         short: '-W PERCENT',
+         description: 'Warn if PERCENT or more of inodes used',
+         proc: proc(&:to_i),
+         default: 85
+
+  option :icrit,
+         short: '-K PERCENT',
+         description: 'Critical if PERCENT or more of inodes used',
+         proc: proc(&:to_i),
+         default: 95
+
+  option :debug,
+         short: '-d',
+         long: '--debug',
+         description: 'Output list of included filesystems'
+
+  # Setup variables
+  #
+  def initialize
+    super
+    @crit_fs = []
+    @warn_fs = []
+  end
+
+  # Get mount data
+  #
+  def fs_mounts
+    Filesystem.mounts.each do |line|
+      begin
+        next if config[:fstype] && !config[:fstype].include?(line.mount_type)
+        next if config[:ignoretype] && config[:ignoretype].include?(line.mount_type)
+        next if config[:ignoremnt] && config[:ignoremnt].include?(line.mount_point)
+        puts "Name: #{ line.name }  Mount Point: #{ line.mount_point }  FS Type: #{ line.mount_type }" if config[:debug]
+      rescue
+        unknown 'An error occured getting the mount info'
+      end
+      @fs_info = Filesystem.stat(line.mount_point)
+      if percent_inodes >= config[:icrit]
+        @crit_fs << "#{line.mount_point} #{percent_inodes}% inode usage"
+      elsif percent_inodes >= config[:iwarn]
+        @warn_fs <<  "#{line.mount_point} #{percent_inodes}% inode usage"
+      end
+      if percent_bytes >= config[:bcrit]
+        @crit_fs << "#{line.mount_point} #{percent_bytes}% bytes usage"
+      elsif percent_bytes >= config[:bwarn]
+        @warn_fs <<  "#{line.mount_point} #{percent_bytes}% bytes usage"
+      end
+    end
+  end
+
+  # Determine the percent inode usage
+  #
+  def percent_inodes
+    (100.0 - (100.0 * @fs_info.inodes_free / @fs_info.inodes)).round(2)
+  end
+
+  # Determine the percent byte usage
+  #
+  def percent_bytes
+    (100.0 - (100.0 * @fs_info.bytes_free / @fs_info.bytes_total)).round(2)
+  end
+
+  # Generate output
+  #
+  def usage_summary
+    (@crit_fs + @warn_fs).join(', ')
+  end
+
+  # Main function
+  #
+  def run
+    fs_mounts
+    critical usage_summary unless @crit_fs.empty?
+    warning usage_summary unless @warn_fs.empty?
+    ok "All disk usage under #{config[:bwarn]}% and inode usage under #{config[:iwarn]}%"
+  end
+end
