fddf7f9f74b34ffacb62dec342c5418db81729f8
Ben Abrams <me@benabrams.it>
2019-01-07 23:41:39 -0800

    prep for release 4.0.0
    Signed-off-by: Ben Abrams <me@benabrams.it>

--- lib/sensu-plugins-disk-checks/version.rb
+++ lib/sensu-plugins-disk-checks/version.rb
@@ -1,7 +1,7 @@
 module SensuPluginsDiskChecks
   module Version
-    MAJOR = 3
-    MINOR = 1
+    MAJOR = 4
+    MINOR = 0
     PATCH = 0
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
