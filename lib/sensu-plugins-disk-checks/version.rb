a8a29bf0ab600c87bc4748f89b47ec27b23f5916
Jef Spaleta <jef@sensu.io>
2019-04-10 15:55:06 -0800

    prepping for 5.0.0 release

--- lib/sensu-plugins-disk-checks/version.rb
+++ lib/sensu-plugins-disk-checks/version.rb
@@ -1,8 +1,8 @@
 module SensuPluginsDiskChecks
   module Version
-    MAJOR = 4
+    MAJOR = 5
     MINOR = 0
-    PATCH = 1
+    PATCH = 0
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
