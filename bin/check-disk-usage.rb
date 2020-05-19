3ed95aca3f395420f3b6576c15ecf17edea209ce
Justin Lewis <juslewis@gmail.com>
2020-05-18 23:13:23 -0700

    Truncate not supported in ruby 2.3

--- bin/check-disk-usage.rb
+++ bin/check-disk-usage.rb
@@ -215,7 +215,7 @@ class CheckDisk < Sensu::Plugin::Check::CLI
       if nonroot_total.zero?
         0
       else
-        (u100 / nonroot_total + (u100 % nonroot_total != 0 ? 1 : 0)).truncate(2)
+        ('%.2f' % (u100 / nonroot_total + (u100 % nonroot_total != 0 ? 1 : 0))).to_f
       end
     else
       (100.0 - (100.0 * fs_info.bytes_free / fs_info.bytes_total)).round(2)
